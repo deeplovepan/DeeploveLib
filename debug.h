48b5fd26863ee497332b68f34685c32a203cf7f6
deeplovepan <deeplovepan@gmail.com>
2010-08-24 15:49:33 +0800

    commit

---
+++ debug.h
@@ -0,0 +1,19 @@
+/*
+ *  debug.h
+ *  DeeploveLib
+ *
+ *  Created by System Administrator on 8/20/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#ifdef DEBUG
+	#define DbgMsg(x...) printf(x)
+#else
+	#define DbgMsg(x...)
+#endif
+
+#endif
