6e6d5e238bd890238caadf32866e972a3e76fc65
deeplovepan <deeplovepan@gmail.com>
2010-09-14 18:41:40 +0800

    commit

---
+++ myDevice.h
@@ -0,0 +1,18 @@
+/*
+ *  myDevice.h
+ *  DeeploveLib
+ *
+ *  Created by System Administrator on 9/14/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef MY_DEVICE_H
+#define MY_DEVICE_H
+
+struct MyDevice {
+	request_queue_t *queue;
+	struct gendisk *disk;
+};
+
+#endif
