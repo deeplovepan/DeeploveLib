b23484dd9836a4c20d413794cc6ed28a0e7f3972
deeplovepan <deeplovepan@gmail.com>
2010-09-21 09:37:24 +0800

    commit

---
+++ linuxHelloDriver.c
@@ -0,0 +1,27 @@
+/*
+ *  linuxHelloDriver.c
+ *  DeeploveLib
+ *
+ *  Created by System Administrator on 9/15/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include <linux/module.h> 
+
+static int hello_init(void) 
+{ 
+	printk(KERN_ALERT "Hello, world\n"); 
+	
+	return 0; 
+} 
+
+
+static void hello_exit(void) 
+{ 
+	printk(KERN_ALERT "Goodbye, cruel world\n"); 
+} 
+
+module_init(hello_init); 
+module_exit(hello_exit); 
+
