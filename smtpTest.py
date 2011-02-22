350f53fb766326dac6fe56fdafd6c04eedb3f1e0
deeplovepan <deeplovepan@gmail.com>
2011-02-22 16:35:12 +0800

    commit

---
+++ smtpTest.py
@@ -0,0 +1,26 @@
+import smtplib
+from email.mime.text import MIMEText
+
+def sendMail(content):
+	smtpServerIp = "192.168.1.18"
+	smtpServerPort = 25
+	loginPassword = ""
+	loginName = ""
+	
+	server = smtplib.SMTP(smtpServerIp, smtpServerPort)
+
+	fromaddr = "abc@infortrend.com"
+	toaddr = "Andy.Pan@infortrend.com"
+	
+	msg = MIMEText(content)
+	msg['Subject'] = "new1"
+	msg['From'] = fromaddr
+	msg['To'] = toaddr
+	
+	if loginPassword != "":
+		server.login(loginName, loginPassword)
+	#server.set_debuglevel(1)
+	server.sendmail(fromaddr, toaddr, msg.as_string())
+	server.quit()
+
+sendMail("hi")
